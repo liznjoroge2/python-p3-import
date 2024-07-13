786aafca6247e205b8dd0539a7386e15f2e9c6c1
enjoroge <njrogeelizabeth53@gmail.com>
2024-07-14 00:58:56 +0300

    complete

--- lib/absolute_package/package1/module1.py
+++ lib/absolute_package/package1/module1.py
@@ -1,2 +1,4 @@
+# relative_package/subpackage1/subpackage2/module1.py
+
 def function1():
-    print('Function 1 in module 1')
+    print('Function 1 from module1')
--- lib/relative_package/subpackage1/module3.py
+++ lib/relative_package/subpackage1/module3.py
@@ -1,2 +1,4 @@
+# relative_package/subpackage1/module3.py
+
 def function1():
     print('Function 1 from module3')
